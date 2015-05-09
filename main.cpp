e1ba4cfcea860f392994002e3e87a208433c0ddf
watulion <rdo_cnt@hotmail.com>
2015-05-09 02:39:19 -0500

    Ya funcionan bien los arreglos de guardar timestamps hasta llenar, falta LRU

--- main.cpp
+++ main.cpp
@@ -2,33 +2,100 @@
 #include <string>
 #include<fstream>
 #include<sstream>
+#include <ctime>
 #include <math.h>
+#include <cmath>
+#include <time.h>
 using namespace std;
+
 
 //para leer archivo de texto
 
 ifstream File;
 string FileRead;
 
+//Timer para LRU
+
+int marcosrealtimestamps[256] = {-1}; //(para calcular tiempo en el que se modifico)
+int cputime = 0;
+
 //variables que manejan marcos
 
-int marcosreal[256]; //(numero de proceso que utiliza el marco)
-int marcosrealmodificado[256]; //(0 o 1, modificado o no)
-int marcosrealtimestamps[256]; //(para calcular tiempo en el que se modifico)
-int marcosvirtual[512]; //(apunta al proceso del marco real que tenia la memoria real)
+int marcosreal[256] = {-1}; //(numero de proceso que utiliza el marco)
+int marcosrealmodificado[256] = {0}; //(0 o 1, modificado o no)
+int marcosvirtual[512] = {-1}; //(apunta al proceso del marco real que tenia la memoria real)
+int cantMarcos = 0;
 
 
 void cargarProceso()
 {
+//Formato (P n p)
+double n = 0;
+int p = 0;
+int residuo = 0; //utilizado para saber cuantos marcos de paginación hay que reemplazar cuando se llene la memoria
+int lleno = 0; // cuando se pase de 256 paginas/marcos, este estara arriba de 1
+
+
+//leer n
+File>>FileRead;
+istringstream (FileRead) >>n;
+
+//leer p
+File>>FileRead;
+istringstream (FileRead) >>p;
+
+//Debug de direcciones
+//cout << "Proceso: " << p << " Direcciones: " << n << endl;
+
+//Empezar a meter datos
+
+int paginas = cantMarcos + ceil (n/8);
+lleno = paginas/256;
+residuo = paginas % 256;
+
+int ultimoMarco = 0;
+
+//Si hay espacio
+if(lleno == 0)
+{
+    ultimoMarco = residuo + cantMarcos - 1;
+    for(int i = cantMarcos; i < ultimoMarco; i++)
+    {marcosrealtimestamps[i] = cputime;
+    marcosreal[i] = p;
+    }
+    cantMarcos += residuo;
+
+}
+else
+{
+    //checar si quedan espacios libres
+    int faltantes;
+    if (cantMarcos < 256)
+    {
+        ultimoMarco = 256;
+        int restanteMarcos = 255-cantMarcos;
+        for(int i = cantMarcos; i < ultimoMarco; i++)
+        {
+          marcosrealtimestamps[i] = cputime;
+          marcosreal[i] = p;
+
+        }
+        cantMarcos += residuo;
+    }
+}
+
+cputime++;
+
+
+//Ver si se superaron los 256 marcos con texto
+cout << "Lleno: " << lleno << " Paginas: " << residuo << " hecho por el proceso: " << marcosreal[ultimoMarco-1]<< " tiempo entrada: " << marcosrealtimestamps[ultimoMarco-1] << endl;
 
 }
 
 int main()
 {
     //Cargar archivo de texto
-    File.open("text.txt");
-	//string FileRead;
-
+    File.open("texto.txt");
     while (!File.eof()) {
 
         File>>FileRead; //agarra cada caracter separado por un espacio.
@@ -39,7 +106,6 @@ int main()
         //else if(FileRead == "E" || FileRead == "e"){E();}
         //else if(FileRead == "F" || FileRead == "f"){F();}
         else{}//cout<<FileRead<<endl;}
-
 
 }//while
     File.close();
